c6754f8378e1df01d045758782cdf82f52807c95
Pascal Welsch <pascal.welsch@gmail.com>
2018-12-17 03:11:14 +0100

    Solve Day 6 - Part 1

---
+++ 06/day6.dart
@@ -0,0 +1,208 @@
+import 'dart:io';
+
+import 'package:dart_kollection/dart_kollection.dart';
+
+main() {
+  final coordinates = listOf(File("06/input.txt").readAsLinesSync())
+      .map(Coordinate.parse)
+      .toSet();
+
+  final finiteCoords = finiteCoordinates(coordinates);
+  final finiteNames = finiteCoords.map((it) => it.name);
+  print(finiteCoords.joinToString());
+
+  final areas = areaOf(coordinates);
+  print(areas);
+  final max = areas.entries
+      .filter((it) => finiteNames.contains(it.key))
+      .maxBy<num>((entry) => entry.value);
+
+  print("max point ${max.key}");
+  print("area ${max.value}");
+  //(max as Coordinate)
+}
+
+final _doubleCoord = Coordinate(-1, -1, " .");
+
+final N = 500;
+
+KMap<String, int> areaOf(KCollection<Coordinate> allCoords) {
+  final KCollection<Coordinate> finiteCoords = finiteCoordinates(allCoords);
+  assert(allCoords.containsAll(finiteCoords));
+  final map = mutableListOf(List.generate(
+      (N), (_) => mutableListOf(List<Coordinate>.filled(N, null))));
+
+  void printGrid() {
+    final buffer = StringBuffer();
+    for (int i = 0; i < map.size; i++) {
+      for (int j = 0; j < map[0].size; j++) {
+        buffer.write(map[j][i]?.name?.padLeft(2) ?? " -");
+      }
+      buffer.write("\n");
+    }
+    print(buffer.toString());
+  }
+
+  for (final coord in allCoords.iter) {
+    map[coord.x][coord.y] = coord;
+  }
+
+  var radius = 0;
+  while (map.flatMap((it) => it).contains(null)) {
+    print("radius $radius");
+    //printGrid();
+    radius++;
+    KMutableList<Coordinate> thisRoundAdded = mutableListOf<Coordinate>();
+
+    for (final coord in allCoords.iter) {
+      final circlePoints = coord.circlePoints(radius);
+      for (final p in circlePoints.iter) {
+        try {
+          var value = map[p.x][p.y];
+          if (value == null) {
+            // add
+            map[p.x][p.y] = coord;
+            thisRoundAdded.add(p);
+          } else {
+            if (thisRoundAdded.contains(p)) {
+              map[p.x][p.y] = _doubleCoord;
+            }
+          }
+        } on IndexOutOfBoundsException {
+          // ignore
+        }
+      }
+    }
+    if (radius > N * 2) {
+      throw "radius > ${N * 2}";
+    }
+  }
+
+  return map
+      .flatMap((it) => it)
+      .groupBy((it) => it.name)
+      .toMutableMap()
+      .mapValues((entry) => entry.value.count());
+}
+
+KList<Coordinate> finiteCoordinates(KCollection<Coordinate> coordinates) {
+  return coordinates.filter((point) {
+    if (point.overflowsRight(coordinates)) {
+      print("$point overflows right");
+      return false;
+    }
+
+    if (point.overflowsLeft(coordinates)) {
+      print("$point overflows left");
+      return false;
+    }
+
+    if (point.overflowsTop(coordinates)) {
+      print("$point overflows top");
+      return false;
+    }
+
+    if (point.overflowsBottom(coordinates)) {
+      print("$point overflows bottom");
+      return false;
+    }
+
+    print("$point is finite");
+    return true;
+  });
+}
+
+class Coordinate {
+  Coordinate(this.x, this.y, [this.name]);
+
+  final int x;
+  final int y;
+  final String name;
+
+  @override
+  String toString() {
+    return '${name ?? ""}($x, $y)';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Coordinate &&
+          runtimeType == other.runtimeType &&
+          x == other.x &&
+          y == other.y &&
+          name == other.name;
+
+  @override
+  int get hashCode => x.hashCode ^ y.hashCode ^ name.hashCode;
+
+  static int _uniqueIndex = 0;
+
+  static Coordinate parse(String input) {
+    final split = input.split(", ");
+    return Coordinate(
+        int.parse(split[0]), int.parse(split[1]), (++_uniqueIndex).toString());
+  }
+
+  KSet<Coordinate> circlePoints(int radius) {
+    var set = linkedSetOf<Coordinate>();
+    // TODO solve with +=
+    set.add(Coordinate(x, y + radius));
+    set.add(Coordinate(x, y - radius));
+    set.add(Coordinate(x + radius, y));
+    set.add(Coordinate(x - radius, y));
+    for (int j = 1; j <= radius - 1; j++) {
+      // TODO solve with +=
+      set.add(Coordinate(x + j, y + radius - j));
+      set.add(Coordinate(x - j, y + radius - j));
+      set.add(Coordinate(x + j, y - radius + j));
+      set.add(Coordinate(x - j, y - radius + j));
+    }
+    set.filter((c) => c.distance(this) == radius);
+    return set;
+  }
+
+  int distance(Coordinate other) {
+    return (x - other.x).abs() + (y - other.y).abs();
+  }
+
+  bool overflowsRight(KCollection<Coordinate> set) {
+    final rightOf = set.filter((coord) {
+      var dx = coord.x - x;
+      var dy = coord.y - y;
+      return dx > 0 && dy.abs() <= dx.abs();
+    });
+
+    return rightOf.isEmpty();
+  }
+
+  bool overflowsLeft(KCollection<Coordinate> set) {
+    final leftOf = set.filter((coord) {
+      var dx = coord.x - x;
+      var dy = coord.y - y;
+      return dx < 0 && dy.abs() <= dx.abs();
+    });
+
+    return leftOf.isEmpty();
+  }
+
+  bool overflowsBottom(KCollection<Coordinate> set) {
+    final bottomOf = set.filter((coord) {
+      var dx = coord.x - x;
+      var dy = coord.y - y;
+      return dy > 0 && dx.abs() <= dy.abs();
+    });
+
+    return bottomOf.isEmpty();
+  }
+
+  bool overflowsTop(KCollection<Coordinate> set) {
+    final topOf = set.filter((coord) {
+      var dx = coord.x - x;
+      var dy = coord.y - y;
+      return dy < 0 && dx.abs() <= dy.abs();
+    });
+
+    return topOf.isEmpty();
+  }
+}
---
+++ 06/day6_test.dart
@@ -0,0 +1,28 @@
+import 'package:dart_kollection/dart_kollection.dart';
+import 'package:test/test.dart';
+
+import 'day6.dart';
+
+main() {
+  group("sample 1", () {
+    var a = Coordinate(1, 1, "A");
+    var b = Coordinate(1, 6, "B");
+    var c = Coordinate(8, 3, "C");
+    var d = Coordinate(3, 4, "D");
+    var e = Coordinate(5, 5, "E");
+    var f = Coordinate(8, 9, "F");
+    final coords = listOf([a, b, c, d, e, f]);
+
+    test("area of D is 9", () {
+      final areas = areaOf(coords);
+      print(areas);
+      expect(areas["D"], 9);
+      expect(areas["E"], 17);
+    });
+
+    test("detect finite coords", () {
+      var finite = finiteCoordinates(coords);
+      expect(finite, listOf([d, e]));
+    });
+  });
+}
